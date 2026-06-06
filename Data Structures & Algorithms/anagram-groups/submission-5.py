2424386d6fe8fb101db67ccfdddd58c5511b2b8f
Devan <146295367+dev-par@users.noreply.github.com>
2026-06-05 21:36:28 -0700

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # group all anagrams into sublists
+        # anagrams are the same when sorted
+        # use the key as the sorted anagram and the value as a list of the anagrams
+        
+        hashmap = defaultdict(list)
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            hashmap[sorted_word].append(word)
+
+        res = []
+        for key, val in hashmap.items():
+            res.append(val)
+        return res
